9e30aaeb3026cf001ce204a11899322ec385ae76
kethlyn kruk <kethlynsk17@outlook.com>
2024-07-01 21:01:40 -0300

    código com função

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")();
 
 let n = +prompt("Escolha o número: ")
 
-n = parseFloat(n.toString().split('').sort((a, b) => b - a).join('')) * Math.sign(n)
+// n = parseFloat(n.toString().split('').sort((a, b) => b - a).join('')) * Math.sign(n)
 
-console.log("Maior número possível: ",n)
 
+function ehNumero(num) {
+    return !isNaN(Number(num));
+}
+
+function maiorNumero(num) {
+    if (ehNumero(num)) {
+    let array = num.split("");
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+        if (array[i] < array[j]) {
+        let aux = array[j];
+        array[j] = array[i];
+        array[i] = aux;
+        }
+    }
+    }
+
+    return array.join("");
+}
+console.log("Número inválido");
+}
+
+const numero = prompt("Digite um número: ");
+
+let maior = maiorNumero(numero);
+console.log(maior);
